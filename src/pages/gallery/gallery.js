46159eae13c0c0935baa9537a57d0ea84d371944
CCDevX <cavaleri.cristina.dev@gmail.com>
2025-04-08 13:11:53 +0200

    feat: design gallery page with filters, responsive layout and preview panel

--- src/pages/gallery/gallery.js
+++ src/pages/gallery/gallery.js
@@ -1,1 +1,16 @@
 import "./gallery.scss";
+
+const galleryItems = document.querySelectorAll(".gallery-item img");
+const previewImage = document.getElementById("preview-image");
+const previewContainer = document.getElementById("gallery-preview");
+const placeholder = document.getElementById("placeholder-message");
+
+console.log(galleryItems);
+
+galleryItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    previewImage.src = item.src;
+    previewContainer.classList.remove("hidden");
+    placeholder.classList.add("inactive");
+  });
+});
