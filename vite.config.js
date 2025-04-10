99ddfc68dabfcb10067ece9d36c8ff66ef401d27
CCDevX <cavaleri.cristina.dev@gmail.com>
2025-04-10 14:27:30 +0200

    build dist

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,20 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
   root: "./src",
-  base: "/",
+  // base: "/",
   build: {
     outDir: "../dist",
     emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/index.html"),
+        homePage: resolve(__dirname, "src/pages/home/home.html"),
+        galleryPage: resolve(__dirname, "src/pages/gallery/gallery.html"),
+        aboutPage: resolve(__dirname, "src/pages/about/about.html"),
+        contactPage: resolve(__dirname, "src/pages/contact/contact.html"),
+      },
+    },
   },
 });
