c806c51b50760e1582f9a25eae0e8237632730be
CCDevX <cavaleri.cristina.dev@gmail.com>
2025-04-10 18:11:19 +0200

    fix bug test

--- dist/common/javascript/helper/page-init.js
+++ dist/common/javascript/helper/page-init.js
@@ -1,12 +1,16 @@
-const initPage = (pageName, params = {}) => {
+const initPage = async (pageName, params = {}) => {
   switch (pageName) {
     case "home":
+      await import("../../../pages/home/home.js");
       break;
     case "gallery":
+      await import("../../../pages/gallery/gallery.js");
       break;
     case "about":
+      await import("../../../pages/about/about.js");
       break;
     case "contact":
+      await import("../../../pages/contact/contact.js");
       break;
     default:
       break;
